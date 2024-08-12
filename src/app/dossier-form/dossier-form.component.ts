1121d9d8fd12f62351bd66febc0e757c2f296109
Benchekhkaouthar <benckaouthar2001@gmail.com>
2024-08-12 16:36:41 +0100

    dossier form component update

--- src/app/dossier-form/dossier-form.component.ts
+++ src/app/dossier-form/dossier-form.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dossier-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule, 
+    MatButtonModule,
+    MatDatepickerModule,
+    MatSelectModule, 
+    MatCardModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './dossier-form.component.html',
   styleUrl: './dossier-form.component.scss'
 })
 export class DossierFormComponent {
+
+  private fb = inject(FormBuilder); 
+  private route = inject(Router);
+ 
+ 
+ form ?: FormGroup;
+
+ cadreAchats = [
+  {value: 'SMA/01/24',name: 'SMA/01/24'},
+  {value: 'SMA/02/24',name: 'SMA/02/24'}
+];
+
+ngOnInit(): void {
+  this.form = this.fb.group({
+    cadreAchat: ['', [Validators.required]],
+    dateFichier: ['', [Validators.required]], 
+    file: ['', Validators.required]
+  });
+}
+
+saveDossier(): void {
+
+}
 
 }
